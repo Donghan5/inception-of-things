71d40dc8e75e4a715cf2d77b2acba4494fe31dae
donghank <donghank@f1br7s8.paris.42.school>
2026-05-15 14:59:41 +0200

    fix: added server-side

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -13,7 +13,7 @@ kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
 kubectl create namespace dev --dry-run=client -o yaml | kubectl apply -f -
 
 echo "=== Applying Argo CD manifest... ==="
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml --server-side
 
 sleep 5
 
